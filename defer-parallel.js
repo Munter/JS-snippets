b868594047597dba8ed576e9c66c62231eb3bb8f
Peter Müller <munter@fumle.dk>
2012-09-21 11:32:47 +0200

    Make defer-parallel AMD compatible

--- defer-parallel.js
+++ defer-parallel.js
@@ -1,56 +1,62 @@
 /*global document*/
-(function () {
-    defer = (function (document, script) {
-        var scripts = {},
-            firstScript = document.getElementsByTagName(script)[0];
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        return factory;
+    } else {
+        root.defer = factory();
+    }
+}(this, function () {
+    var document = document,
+        script = 'script',
+        scripts = {},
+        firstScript = document.getElementsByTagName(script)[0];
 
-        return function (urls, callback) {
-            var ringRing = function () {
-                    var callMe = true;
-                    urls.forEach(function (url) {
-                        if (typeof scripts[url] !== 'undefined' && !scripts[url].onloadDone) {
-                            callMe = false;
-                        }
-                    });
-                    if (callMe) {
-                        callback();
+    return function (urls, callback) {
+        var ringRing = function () {
+                var callMe = true;
+                urls.forEach(function (url) {
+                    if (typeof scripts[url] !== 'undefined' && !scripts[url].onloadDone) {
+                        callMe = false;
                     }
-                },
-                callMe = callback && true;
-
-            if (typeof urls === 'string') {
-                urls = [urls];
-            }
-
-            urls.forEach(function (url) {
-                var inc;
-
-                if (typeof scripts[url] === 'undefined') {
-                    callMe = false;
-                    inc = document.createElement(script);
-                    inc.async = true;
-                    inc.src = url;
-                    if (callback) {
-                        inc.onload = function () {
-                            if (!inc.onloadDone) {
-                                inc.onloadDone = true;
-                                ringRing();
-                            }
-                        };
-                        inc.onreadystatechange = function () {
-                            if (inc.readyState === "loaded" || inc.readyState === "complete") {
-                                inc.onload();
-                            }
-                        };
-                    }
-                    firstScript.parentNode.insertBefore(inc, firstScript);
-                    scripts[url] = inc;
+                });
+                if (callMe) {
+                    callback();
                 }
-            });
+            },
+            callMe = callback && true;
 
-            if (callMe) {
-                ringRing();
+        if (typeof urls === 'string') {
+            urls = [urls];
+        }
+
+        urls.forEach(function (url) {
+            var inc;
+
+            if (typeof scripts[url] === 'undefined') {
+                callMe = false;
+                inc = document.createElement(script);
+                inc.async = true;
+                inc.src = url;
+                if (callback) {
+                    inc.onload = function () {
+                        if (!inc.onloadDone) {
+                            inc.onloadDone = true;
+                            ringRing();
+                        }
+                    };
+                    inc.onreadystatechange = function () {
+                        if (inc.readyState === "loaded" || inc.readyState === "complete") {
+                            inc.onload();
+                        }
+                    };
+                }
+                firstScript.parentNode.insertBefore(inc, firstScript);
+                scripts[url] = inc;
             }
-        };
-    }(document, 'script'));
-}());
+        });
+
+        if (callMe) {
+            ringRing();
+        }
+    };
+}));
