c12fb1d062f91b6e7e4548c9b647e93496d15549
Peter Müller <munter@fumle.dk>
2012-09-21 11:33:55 +0200

    Make defer-callback AMD compatible

--- defer-callback.js
+++ defer-callback.js
@@ -1,36 +1,42 @@
 /*global document*/
-(function () {
-    defer = (function (document, script) {
-        var scripts = {},
-            firstScript = document.getElementsByTagName(script)[0];
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        return factory;
+    } else {
+        root.defer = factory();
+    }
+}(this, function () {
+    var document = document,
+        script = 'script',
+        scripts = {},
+        firstScript = document.getElementsByTagName(script)[0];
 
-        return function (url, callback) {
-            var inc;
+    return function (url, callback) {
+        var inc;
 
-            if (typeof scripts[url] === 'undefined') {
-                inc = document.createElement(script);
-                inc.async = true;
-                inc.src = url;
-                if (callback) {
-                    inc.onload = function () {
-                        if (!inc.onloadDone) {
-                            inc.onloadDone = true;
-                            callback();
-                        }
-                    };
-                    inc.onreadystatechange = function () {
-                        if (inc.readyState === "loaded" || inc.readyState === "complete") {
-                            inc.onload();
-                        }
-                    };
-                }
-                firstScript.parentNode.insertBefore(inc, firstScript);
-                scripts[url] = inc;
-            } else {
-                if (callback) {
-                    callback();
-                }
+        if (typeof scripts[url] === 'undefined') {
+            inc = document.createElement(script);
+            inc.async = true;
+            inc.src = url;
+            if (callback) {
+                inc.onload = function () {
+                    if (!inc.onloadDone) {
+                        inc.onloadDone = true;
+                        callback();
+                    }
+                };
+                inc.onreadystatechange = function () {
+                    if (inc.readyState === "loaded" || inc.readyState === "complete") {
+                        inc.onload();
+                    }
+                };
             }
-        };
-    }(document, 'script'));
-}());
+            firstScript.parentNode.insertBefore(inc, firstScript);
+            scripts[url] = inc;
+        } else {
+            if (callback) {
+                callback();
+            }
+        }
+    };
+}));
